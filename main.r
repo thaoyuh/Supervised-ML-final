9149ab6581285a030f2f76c318be3a0742954928
Thao Le <thaole26601@gmail.com>
2022-12-09 18:23:16 +0100

    finalize code

--- main.r
+++ main.r
@@ -1,174 +1,117 @@
-# Setup -------------------------------------------------------------------
+#------------------------------------
+#          Set up           
+#------------------------------------
 # load packages
 if(!require(pacman)){install.packages("pacman")}
-p_load(tidyverse, simglm, rlist, latex2exp, glmnet, knitr, formatR, devtools, glmnetUtils,rpart)
+p_load(tidyverse, simglm, rlist, latex2exp, 
+       glmnet, knitr, formatR, devtools, glmnetUtils,rpart,
+       caret,parallel,functional, ipred,rattle,rpart.plot)
 
 # set seed
 set.seed(1234)
 
-# import custom functions
+# import manual functions
 source("./dev/toolbox.r")
 
-# writing format objects
-mytheme <- theme_bw() + theme(legend.position = "bottom")
-
-#----------------Preparing data
-#MAKE FUNCTION
-# Import dataset
+#------------------------------------
+#          Data Preparation           
+#------------------------------------
+# Load data
 cleaned_data <- read_csv("cleaned_data.csv")
 df <- data.frame(cleaned_data) 
 df[is.na(df)] <- 0
+
+# Split train and test data
 ind <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
 df_train  <- df[ind, ]
 df_test   <- df[!ind, ]
 
 mXTrain = subset(df_train, select=-c(fraudulent)) %>% as.matrix()
-vYTrain = as.vector(df_train$fraudulent)      # y variable
-
+vYTrain = as.vector(df_train$fraudulent)    
 mXTest = subset(df_test, select=-c(fraudulent)) %>% as.matrix()
-vYTest = as.vector(df_test$fraudulent)      # y variable
+vYTest = as.vector(df_test$fraudulent)    
 
-#----------------------------------Logistic Regression
-#--------------CV for optimal alpha and lamda
-# MAKE FUNCTION
-# Cross validate for optimal alpha
-
-
+#------------------------------------
+#         Logistic Regression            
+#------------------------------------
+# Initialize
 lLambda=10^seq(-10, 5, length.out = 50)
 lAlpha = seq(0, 1, length.out = 50)
 nfolds = 10
 
-CV_lambda_alpha = function(mExplainVars, vFraudulent, lLambda, lAlpha, nfolds){
-  # Initialize
-  lErrorMin = list()
-
-  # mError = matrix(NA, nrow=length(lLambda), ncol=length(lAlpha)) #matrix with nrows = number of lamda and ncols = number of alpha
-  
-  # Perform validation
-  for (i in 1:length(lAlpha)){
-    Cv_result = cv.glmnet(mExplainVars, vFraudulent, alpha = lAlpha[i], 
-                          lambda = lLambda, nfolds = nfolds,family = "binomial",type.measure = "class")
-    
-    # Get minimum error
-    lError = Cv_result$cvm
-    #mError[,i] = lError
-    dMinError = min(lError)
-    lErrorMin[i] = dMinError
-    
-    # # Get minimum lambda
-    # iIndex = which.min(Cv_result$cvm)
-
-  }
-  # List of return values
-  
-  return(lErrorMin)
-}
-
-
+# Cross validate for optimal alpha and lambda
 lResult= CV_lambda_alpha(mXTrain, vYTrain, lLambda, lAlpha, nfolds)
 
-lErrorMin=lResult[[1]]
+# Get most and least optimal alphas
+dAlphaMin = lAlpha[which.min(lResult)] # Most optimal
+dAlphaMax = lAlpha[which.max(lResult)] # Least optimal
 
-# Get minimum and maximum alpha
-# alpha=1 is lasso regression (default) and alpha=0 is ridge regression
-iIndexMin = which.min(lErrorMin)
-iIndexMax = which.max(lErrorMin)
-dAlphaMin = lAlpha[iIndexMin] #1
-dAlphaMax = lAlpha[iIndexMax] #0
+#' Keeping fixed most and least optimal alphas, 
+#' compare difference in cross-validated errors over lambdas.
+Cv_res = compare_alphas_cv(mXTrain, vYTrain, dAlphaMin, dAlphaMax, lLambda)
+Cv_min =Cv_res[[1]]
+cat("best CV lambda", Cv_min$lambda.min)
+cat("best CV lambda 1SE", Cv_min$lambda.1se)
 
 
-# MAKE FUNCTION
-#Elastic net using the optimal alpha value
-Cv_min <- cv.glmnet(mXTrain, vYTrain, alpha = dAlphaMin, 
-                     lambda = 10^seq(-10, 10, length.out = 50), nfolds = 10, family = "binomial",type.measure = "class")  
-Cv_max <- cv.glmnet(mXTrain, vYTrain, alpha = dAlphaMax, 
-                    lambda = 10^seq(-10, 10, length.out = 50), nfolds = 10, family = "binomial",type.measure = "class")  
-print(Cv_min$lambda.min)    
-print(Cv_min$lambda.1se)   
-
-par(mfrow = c(1,2))
-plot(Cv_min); plot(Cv_max)
-
-# Final run with best cross validated lambda
-coef(Cv_min, s = "lambda.min")
-
-### MAKE FUNCTION
-#Plot the minimum misclassification rate over Alphas
-par(mfrow = c(1,1))
-plot(x =lAlpha, y=lErrorMin,yaxt='n', xlab= "Alpha", ylab= "", col="red", pch=10)
-ylabel <- seq(min(unlist(lErrorMin)), max(unlist(lErrorMin)), by = 0.0001) %>%round( digits = 4)
-axis(2, at = ylabel,las=0)
-title(ylab="Minimum Classification Error", line=2.7, cex.lab=1)
-
-#-----------------Prediction and evaluations Logistic regression
-# Summarize the model
-summary(Cv_min)
-# Make predictions
+# Prediction
+vBeta=coef(Cv_min, s = "lambda.min")
 probabilities <- Cv_min %>% predict(mXTest, type = "response")
 log_predictions <- ifelse(probabilities > 0.5, 1, 0)
-# Model accuracy
-mean(log_predictions == vYTest)
 
-# Confusion matrix
-confusionMatrix(table(vYTest, log_predictions))
+# Evaluation
+log_evaluations = model_evaluate(log_predictions, vYTest)
 
-#calculate sensitivity
-sensitivity=sensitivity(table(vYTest, log_predictions))
+#------------------------------------
+#            Decision Tree            
+#------------------------------------
 
-#calculate specificity
-specificity=specificity(table(vYTest, log_predictions))
+# Get the cp value of optimal pruned tree using train data
+fit = pruned_tree(df_train)
+ptree = fit[[1]]
+cp_optimal = fit[[2]]
 
-# False Positive Rate
-FPR <- 1-specificity
-# True positive Rate
-TPR <- sensitivity
+# Predictions on test set
+tree_predictions = predict(ptree, df_test, type = 'class')
+
+# Evaluation
+tree_evaluation = model_evaluate(tree_predictions, vYTest)
+
+#------------------------------------
+#      Bagging Manual Algorithm             
+#------------------------------------
+iNTree= 500
+
+#Get predictions and our of bag errors for 500 bagged samples in parallel with 8 cores
+lBagResult = mclapply(1: iNTree, function(i) {bagged_tree_manual(df_train, df_test)}, 
+                      mc.cores = parallel::detectCores(), mc.set.seed = TRUE)
+
+#extract list of OOB Error in parallel
+lOOB_error = mclapply(1:length(lBagResult), function(i){extract_OOB_error(lBagResult[[i]])}, 
+                      mc.cores = parallel::detectCores(), mc.set.seed = TRUE)
+
+#extract list of predictions in parallel
+lPredictions = mclapply(1:length(lBagResult), function(i){extract_bag_pred(lBagResult[[i]])}, 
+                        mc.cores = parallel::detectCores(), mc.set.seed = TRUE)
+
+# Get predictions
+bagging_predictions_manual = manual_predictions_bagging(lPredictions, df_test)
+
+# Evaluations
+bag_evaluations_manual = model_evaluate(bagging_predictions_manual, vYTest)
+
+#------------------------------------
+#       Bagging Using Package             
+#------------------------------------
+# Predict on test set
+bag_predictions = bagged_package(df_train, df_test, iNTree, cp_optimal)
+
+# Evaluations
+bag_evaluations_package = model_evaluate(bag_predictions, vYTest)
 
 
-#----------------------DECISION TREE
-# Create and cross validate tree https://www.edureka.co/blog/implementation-of-decision-tree/ 
 
-library("rpart")
-library("rattle")
-library(rpart.plot)
-library(RColorBrewer)
-library(caret)
 
-tree <- rpart(fraudulent ~ ., data = df_train,method = 'class')
-
-rpart.plot(tree)
-
-# 10-fold CV with printcp
-printcp(tree)
-
-plotcp(tree)
-
-#Get pruned tree
-ptree = rpart(fraudulent ~ ., data = df_train,cp= tree$cptable[which.min(tree$cptable[,"xerror"]),"CP"],method = 'class')
-fancyRpartPlot(ptree, uniform=TRUE,
-                 main="Pruned Classification Tree")
-
-# Get classification error: Root node error x Rel error
-classification_err= 0.4934*0.43324
-classification_err
-
-tree_predictions <- predict(ptree, df_test, type = 'class')
-
-# Model accuracy
-mean(tree_predictions == vYTest)
-
-# Confusion matrix
-confusionMatrix(table(vYTest, tree_predictions))
-
-#calculate sensitivity
-sensitivity=sensitivity(table(vYTest, log_predictions))
-
-#calculate specificity
-specificity=specificity(table(vYTest, log_predictions))
-
-# False Positive Rate
-FPR <- 1-specificity
-# True positive Rate
-TPR <- sensitivity
 
 
 
